c0e5a16c4114f6b7d9daa07a7bcb28d522ed58b5
Yang Zhao <YangZhao11@users.noreply.github.com>
2015-05-31 20:16:47 -0700

    Revert "Fine tuning God-mode config"
    This reverts commit bbffa196fea1a31a746c3ea098ea34acb7565d20.

--- lisp/deadcode.el
+++ lisp/deadcode.el
@@ -92,28 +92,28 @@
   )
 (global-set-key (kbd "M-m SPC") 'hydra-mark/body)
 
-(defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
-                           :color pink
-                           :hint nil
-                           :post (deactivate-mark))
-  "
-  ^_p_^     _d_elete    s_t_ring  e_x_change
-_b_   _f_   _k_ill      _y_ank    _/_undo
-  ^_n_^     ne_w_-copy  _r_eset   _q_uit
-"
-  ("b" backward-char)
-  ("f" forward-char)
-  ("p" previous-line)
-  ("n" next-line)
-  ("x" z-exchange-point-and-mark)
-  ("w" copy-rectangle-as-kill)
-  ("d" delete-rectangle)
-  ("r" (if (region-active-p)
-           (deactivate-mark)
-         (rectangle-mark-mode 1)))
-  ("y" yank-rectangle)
-  ("/" undo)
-  ("t" string-rectangle)
-  ("k" kill-rectangle)
-  ("q" nil))
-(global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
+;; (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+;;                            :color pink
+;;                            :hint nil
+;;                            :post (deactivate-mark))
+;;   "
+;;   ^_p_^     _d_elete    s_t_ring  e_x_change
+;; _b_   _f_   _k_ill      _y_ank    _/_undo
+;;   ^_n_^     ne_w_-copy  _r_eset   _q_uit
+;; "
+;;   ("b" backward-char)
+;;   ("f" forward-char)
+;;   ("p" previous-line)
+;;   ("n" next-line)
+;;   ("x" z-exchange-point-and-mark)
+;;   ("w" copy-rectangle-as-kill)
+;;   ("d" delete-rectangle)
+;;   ("r" (if (region-active-p)
+;;            (deactivate-mark)
+;;          (rectangle-mark-mode 1)))
+;;   ("y" yank-rectangle)
+;;   ("/" undo)
+;;   ("t" string-rectangle)
+;;   ("k" kill-rectangle)
+;;   ("q" nil))
+;; (global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
