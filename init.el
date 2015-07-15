fef986de99c8246a7496a22afa0ba9d4ceb65c96
Yang Zhao <YangZhao11@users.noreply.github.com>
2015-07-15 10:43:08 -0700

    make link look same as org-link

--- init.el
+++ init.el
@@ -105,4 +105,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(link ((t (:foreground "#D0BF8F" :underline t :weight normal)))))
