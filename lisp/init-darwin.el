90060eaf1cf7b26ac50da94d8a70fa2d3baea3b4
Yang Zhao <YangZhao11@users.noreply.github.com>
2015-07-17 14:04:23 -0700

    Swap left command and option on mac

--- lisp/init-darwin.el
+++ lisp/init-darwin.el
@@ -1,1 +1,5 @@
 (set-frame-font "Menlo-15" nil 't)
+(setq mac-option-modifier 'super
+      mac-command-modifier 'meta
+      mac-right-option-modifier 'meta
+      mac-right-command-modifier 'super)
