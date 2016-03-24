33c73438d4050fb41b7fe89836d8465687348994
Yang Zhao <YangZhao11@users.noreply.github.com>
2016-03-23 22:52:34 -0700

    zap-to-char-dwim

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -360,10 +360,56 @@ in ctl-j-map first."
         aw-background nil
         aw-keys '(?s ?d ?f ?j ?i ?o ?g ?h ?a ?k ?l ?\;)))
 
-(use-package misc
-  :commands zap-up-to-char
-  :bind (("M-z" . zap-up-to-char)
-         ("M-Z" . zap-to-char)))
+(use-package misc :commands zap-up-to-char)
+
+(defun zap-to-char-matching-pairs (arg char)
+  (let ((direction (if (>= arg 0) 1 -1))
+        (start (point)))
+    (search-forward (char-to-string char) nil nil direction)
+    (while (>= (let ((p (point)))
+                 (backward-sexp direction)
+                 (prog1 (point) (goto-char p))) start)
+      (search-forward (char-to-string char) nil nil direction))
+    (backward-char direction)
+    (kill-region start (point))))
+
+(defun zap-to-space (arg)
+  (let ((direction (if (>= arg 0) 1 -1))
+        (on-space (if (>= arg 0)
+                      (looking-at "\\s ")
+                    (looking-back "\\s " (1- (point)))))
+        (sep-regex (if (>= arg 0) "\\S \\s " "\\s \\S "))
+        (start (point)))
+    (when on-space
+      (re-search-forward "\\S " nil nil direction)
+      (backward-char direction))
+    (re-search-forward sep-regex nil nil arg)
+    (backward-char direction)
+    (unless on-space
+      (re-search-forward "\\S " nil nil direction)
+      (backward-char direction))
+    (kill-region start (point))))
+
+(defun zap-to-char-dwim (arg char)
+  "Like zap-up-to-char, except:
+- Press M-z again for zap-to-char
+- zap to space handles consequtive spaces
+- zap to right parenthses goes to the end of enclosing list"
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     (read-char "Zap to: " 't)))
+  (cond
+   ((eq char ?\M-z)
+    (setq this-original-command 'zap-to-char
+          this-command 'zap-to-char
+          real-this-command 'zap-to-char)
+    (call-interactively #'zap-to-char))
+   ((eq (char-syntax char) (if (< arg 0) ?\( ?\)))
+    (zap-to-char-matching-pairs arg char))
+   ((eq char ?\ )
+    (zap-to-space arg))
+   ('t
+    (zap-up-to-char arg char))))
+(bind-keys ("M-z" . zap-to-char-dwim))
 
 (use-package smex :ensure
   :bind ("M-x" . smex))
