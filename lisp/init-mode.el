bf1bb08772c38e1b5e876b04d53cfc5c56c408fa
Yang Zhao <YangZhao11@users.noreply.github.com>
2016-04-14 11:35:56 -0700

    smerge config

--- lisp/init-mode.el
+++ lisp/init-mode.el
@@ -1,10 +1,42 @@
 ; -*- coding: utf-8 -*-
 
+(eval-when-compile (require 'use-package))
+(eval-when-compile (require 'hydra))
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'text-mode-hook #'abbrev-mode)
 (add-hook 'text-mode-hook #'auto-fill-mode)
 (add-hook 'text-mode-hook #'flyspell-mode)
+
+(use-package smerge-mode
+  :bind ("C-x m" . smerge-hydra/body)
+  :config
+  (defhydra smerge-hydra (:color red :hint nil)
+    "
+^Move^      ^Keep^         ^Diff^      ^Pair^
+------------------------------------------------------
+_n_ext      _b_ase         _R_efine    _<_: base-mine
+_p_rev      _m_ine         _E_diff     _=_: mine-other
+^ ^         _o_ther        _C_ombine   _>_: base-other
+^ ^         _a_ll          _r_esolve
+_q_uit      _RET_: current
+"
+    ("RET"       smerge-keep-current)
+    ("C"         smerge-combine-with-next)
+    ("E"         smerge-ediff)
+    ("R"         smerge-refine)
+    ("a"         smerge-keep-all)
+    ("b"         smerge-keep-base)
+    ("m"         smerge-keep-mine)
+    ("n"         smerge-next)
+    ("o"         smerge-keep-other)
+    ("p"         smerge-prev)
+    ("r"         smerge-resolve)
+    ("<"       smerge-diff-base-mine)
+    ("="       smerge-diff-mine-other)
+    (">"       smerge-diff-base-other)
+    ("q" nil :color blue)))
 
 (use-package magit
   :bind ("C-x g" . magit-status)
