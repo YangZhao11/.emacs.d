8c3507fdbd11270ef56ac80b332a4b6baf5b8a3b
Yang Zhao <YangZhao11@users.noreply.github.com>
2018-07-07 09:46:25 -0700

    misc settings

--- lisp/init-darwin.el
+++ lisp/init-darwin.el
@@ -8,6 +8,11 @@
 (setenv "PATH" (concat "/usr/local/git/current/bin:/usr/local/bin:"
                        (getenv "PATH")))
 (setenv "LANG" "en_US.UTF-8")
+
+;; (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+;; (add-to-list 'default-frame-alist '(ns-appearance . dark))
+;; (setq ns-use-proxy-icon nil)
+
 ;; (set-fontset-font t 'unicode
 ;;                   (font-spec :name "Apple Color Emoji" :size 11) nil 'prepend)
 (add-hook 'after-init-hook
