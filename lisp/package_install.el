80aa39235b673c48057f59319f045d274575015a
Yang Zhao <zhyang@google.com>
2023-09-05 10:51:51 -0700

    old content

--- lisp/package_install.el
+++
@@ -1,7 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("GELPA" . "http://internal-elpa.appspot.com/packages/"))
-(package-refresh-contents)
-
-(dolist (p package-selected-packages)
-  (package-install p))
